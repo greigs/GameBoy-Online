b7ff8ce72dd4d6d34df724cd0cba5398ec054885
greigs <greig2@gmail.com>
2019-10-11 02:12:37 +0100

    hightlight active game

--- js/main.js
+++ js/main.js
@@ -1,32 +1,60 @@
 
-const columnCount = 6
-const rowCount = 1
+const columnCount = 5
+const rowCount = 3
 const canvasHeightIndividualGame = 144 * 2
 const canvasWidthIndividualGame = 160 * 2
-const canvasHeightTotal = canvasHeightIndividualGame * rowCount
-const canvasWidthTotal = canvasWidthIndividualGame * columnCount
+const borderSize = 2
+const canvasHeightTotal = (canvasHeightIndividualGame * rowCount) + (rowCount * borderSize) + borderSize
+const canvasWidthTotal = (canvasWidthIndividualGame * columnCount) + (columnCount * borderSize) + borderSize
 let canvas = document.getElementById("canvas")
+canvas.style.width = '100%'
+canvas.style.maxWidth = 'fit-content'
 canvas.width = "" + canvasWidthTotal
 canvas.height = "" + canvasHeightTotal
 var ctx = canvas.getContext('2d')
 ctx.imageSmoothingEnabled = false;
 const offScreen = new OffscreenCanvas(160, 144)
 const offScreenCtx = offScreen.getContext("2d")
+
 let workers = []
 
 for (let i=0; i<columnCount; i++){
 	for (let j=0; j<rowCount; j++){
 		const worker = new Worker('js/worker.js')
-		workers.push(worker)
+		workers[(columnCount * j) + i] = worker
 		worker.postMessage({ i: i, j: j, offsetDistanceX : canvasWidthIndividualGame, offsetDistanceY : canvasHeightIndividualGame});
 		worker.onmessage = function (e) {
 			offScreenCtx.putImageData(e.data.image, 0, 0);
-			ctx.drawImage(offScreen, e.data.i, e.data.j, canvasWidthIndividualGame, canvasHeightIndividualGame);
+			let ia =  (e.data.i / canvasWidthIndividualGame)
+			let x = (ia * canvasWidthIndividualGame) + (ia * borderSize) + borderSize
+			let ja =  (e.data.j / canvasHeightIndividualGame)
+			let y = (ja * canvasHeightIndividualGame) + (ja * borderSize) + borderSize
+			let arrayPos = (ja * columnCount) + ia
+			
+			// if (arrayPos === activeIndex - 1 && activeFrame === (activeIndex - 1)){
+			// 	ctx.lineWidth = borderSize * 2
+			// 	ctx.strokeStyle = 'black';
+			// 	ctx.beginPath();
+			// 	ctx.rect(x, y, canvasWidthIndividualGame, canvasHeightIndividualGame);
+			// 	ctx.stroke();
+			// }				
+
+			if (arrayPos === activeIndex && activeFrame !== activeIndex){				
+				ctx.lineWidth = borderSize * 2
+				ctx.strokeStyle = 'lime';
+				ctx.beginPath();
+				ctx.rect(x, y, canvasWidthIndividualGame, canvasHeightIndividualGame);
+				ctx.stroke();
+				activeFrame = activeIndex
+			}	
+			
+			ctx.drawImage(offScreen, x, y, canvasWidthIndividualGame, canvasHeightIndividualGame);
 		}
 	}
 }
 
 let activeIndex = 0
+let activeFrame = activeIndex
 let active = workers[activeIndex]
 
 let globalFrameCount = 0
@@ -44,7 +72,7 @@ const frameDivisionAll = 5
 const frameDivisionSingle = 1
 const frameDivisionBackground = 10
 const tickInverval = 8
-const changeWorkerInterval = 5000
+const changeWorkerInterval = 3000
 
 setInterval(async () => 
 {
@@ -63,12 +91,11 @@ setInterval(async () =>
 },tickInverval)
 
 
-setInterval(() =>{
-  if (activeIndex === columnCount - 1){
+setInterval(() =>{	
+  activeIndex++
+  if (activeIndex === (columnCount * rowCount) - 1){
     activeIndex = 0
   }
-  else{
-	activeIndex++
-  }
+  
   active = workers[activeIndex]
 }, changeWorkerInterval)
